f450e4476b2abd8dd552f6cd36fbc93fdd6ff868
Colm Laro <smartman97@gmail.com>
2016-02-19 09:11:27 -0700

    Started setting up panel

--- src/array/view/ArrayPanel.java
+++ src/array/view/ArrayPanel.java
@@ -1,6 +1,43 @@
 package array.view;
 
-public class ArrayPanel
-{
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 
+import array.controller.ArrayController;
+
+public class ArrayPanel extends JPanel
+{
+	private ArrayController baseController;
+	private SpringLayout baseLayout;
+	private JTextField rowField;
+	private JTextField colField;
+	private JLabel currentPlayer;
+	private JTextField editField;
+	private JButton changeButton;
+	private JButton displayButton;
+	private JTable playerTable;
+	
+	public ArrayPanel(ArrayController baseController)
+	{
+		this.baseController = baseController;
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
 }
